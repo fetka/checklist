cd96c5a951537edbe59a7a7616eb8718bf74f4ea
mfetka <mfetka@gmail.com>
2022-02-15 13:07:00 +0100

    First commit

---
+++ dom.js
@@ -0,0 +1,66 @@
+const checkedList = JSON.parse(localStorage.getItem(STORAGE_NAME)) || [];
+// console.log(checkedList);
+// console.log(localStorage.getItem(STORAGE_NAME));
+
+// const saveBtn = document.querySelector('#save__button');
+const clearBtn = document.querySelector('#clear__button');
+const resetBtn = document.querySelector('#reset__button');
+const store_name = document.querySelector('#store_name');
+store_name.innerHTML += `${STORAGE_NAME}`;
+
+// saveBtn.addEventListener('click', () => {
+//   localStorage.setItem(STORAGE_NAME, JSON.stringify(checkedList));
+// });
+
+// clearBtn.addEventListener('click', () => {
+//   localStorage.setItem(STORAGE_NAME, JSON.stringify([]));
+// });
+
+(function () {
+  let counter_for_id = 1;
+  checkboxList.forEach((cb) => {
+    const cp_id = cb.name + '_' + counter_for_id++;
+    const done = checkedList.includes(cp_id) ? 'checked' : '';
+
+    function items(items) {
+      if (!items) return '';
+      let builder = '';
+      items.forEach((item) => {
+        builder += `<li>${item}</li>`;
+      });
+      return builder;
+    }
+
+    document.querySelector('app').insertAdjacentHTML(
+      'beforeend',
+      `<input class="w3-check" type="checkbox" id="${cp_id}" name="${cp_id}" 
+      value="${cb.value}" ${done}>
+          <label for="${cb.name}"><b><u> ${cb.text} </u></b></label><br> <ul>${items(
+        cb.items
+      )}</ul>`
+    );
+  });
+})();
+
+const cbElements = document.querySelectorAll('.w3-check');
+
+cbElements.forEach((el) => {
+  el.addEventListener('change', (ev) => {
+    if (ev.target.checked && !checkedList.includes(ev.target.id)) {
+      checkedList.push(ev.target.id);
+    } else {
+      const idx = checkedList.indexOf(ev.target.id);
+      if (idx !== -1) checkedList.splice(idx, 1);
+    }
+    localStorage.setItem(STORAGE_NAME, JSON.stringify(checkedList));
+    // console.log(checkedList);
+  });
+});
+
+resetBtn.addEventListener('click', () => {
+  cbElements.forEach((el) => {
+    el.checked = false;
+  });
+  checkedList.length = 0;
+  localStorage.setItem(STORAGE_NAME, JSON.stringify([]));
+});
---
+++ properties.js
@@ -0,0 +1,8 @@
+const STORAGE_NAME = 'stored_progress_state_2';
+const checkboxList = [
+  { name: 'name', value: 'value', text: 'text 1', items: ['item 1', 'item 2'] },
+  { name: 'name', value: 'value', text: 'text 2', items: ['item 1'] },
+  { name: 'name', value: 'value', text: 'text 3', items: ['item 1', 'item 2'] },
+  { name: 'name', value: 'value', text: 'text 4' },
+  { name: 'name', value: 'value', text: 'text 5', items: ['item 1', 'item 2'] },
+];
